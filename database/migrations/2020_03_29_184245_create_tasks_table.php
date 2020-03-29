cb8caead4c4996a40448ceb6edeaff8379e90f5c
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-30 03:09:55 +0800

    created polymorphic type [comments]

--- database/migrations/2020_03_29_184245_create_tasks_table.php
+++ database/migrations/2020_03_29_184245_create_tasks_table.php
@@ -19,8 +19,9 @@ class CreateTasksTable extends Migration
             $table->integer('project_id')->unsigned();
             $table->integer('company_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->integer('days')->unsigned();
-            $table->integer('hours')->unsigned();
+            $table->integer('days')->unsigned()->nullable();
+            $table->integer('hours')->unsigned()->nullable();
+
             $table->foreign('project_id')->references('id')->on('projects');
             $table->foreign('company_id')->references('id')->on('companies');
             $table->foreign('user_id')->references('id')->on('users');
