b5928f936ea6aafc174041860b71c13dedc242b9
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-30 02:35:45 +0800

    create projects migration

--- database/migrations/2020_03_29_182612_create_projects_table.php
+++ database/migrations/2020_03_29_182612_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCompaniesTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateCompaniesTable extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->longText('description');
+            $table->integer('company_id')->unsigned();
             $table->integer('user_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('companies');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
@@ -30,6 +32,6 @@ class CreateCompaniesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('projects');
     }
 }
