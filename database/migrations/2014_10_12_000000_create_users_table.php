64ccf3e079da1cc5cc94db7db24d6fd402b91dc4
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-30 03:38:52 +0800

    created relationship model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,17 +18,15 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::table('users', function (Blueprint $table) {
             $table->string('first_name')->nullable();
             $table->string('middle_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('city')->nullable();
-            $table->integer('role_id')->unsigned();
+            $table->integer('role_id')->unsigned()->nullable();
+            $table->rememberToken();
+            $table->timestamps();
         });
+
     }
 
     /**
