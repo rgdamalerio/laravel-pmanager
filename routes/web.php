b9163c297e6eed0d73172f1a4513cee76969fb46
ROEL G. DAMALERIO <damalerioroel@gmail.com>
2020-03-30 19:44:39 +0800

    Created controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('companies','CompaniesController');
+Route::resource('projects','ProjectsController');
+Route::resource('roles','RolesController');
+Route::resource('tasks','TasksController');
+Route::resource('users','UsersController');
